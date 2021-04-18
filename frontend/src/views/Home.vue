ee5aa3c0e1dfc0fb504d533c36cdd028bcea2b35
dasyx <seanpnn00@gmail.com>
2021-04-18 13:15:45 +0200

    first commit

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,66 @@
+<template>
+<div class="home">
+  <section class="hero is-dark">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title">
+          Groupomania          
+        </h1>
+        <h2 class="subtitle">
+          Entre nous, c'est entre vous...
+        </h2>
+        <div class="button-block">
+          <button class="button is-xl is-dark">Inscrivez-vous</button>
+        </div>
+      </div>
+    </div>
+  </section>
+</div>
+</template>
+<script>
+export default {
+  name: 'home',
+  components: {
+  }
+}
+</script>
+<style lang="scss" scoped>
+  .hero {    
+    text-align: center;
+    background-image: url('../../public/img/earth_spaceview.jpg');
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    height: 400px;
+  }
+  .hero-body .title {
+    text-shadow: 4px 4px 4px rgba(0, 0, 0, 0.6);
+    padding: 40px 0 20px 0;
+    font-size: 60px;
+  }
+  .subtitle {
+    text-shadow: 4px 4px 4px rgba(0, 0, 0, 0.7);
+    font-size: 30px;
+  }
+  .button-block {
+    text-align: center;
+    margin-left: auto;
+    margin-right: auto;
+    width: 100%;
+    position: absolute;
+    bottom: -150px;
+    .button {
+      padding-left: 50px;
+      padding-right: 50px;
+    }
+    .welcome {
+      width: 400px;
+      padding: 10px;
+      margin-left: auto;
+      margin-right: auto;
+    }
+  }
+  .is-xl {
+    font-size: 1.7rem;
+  }
+</style>
