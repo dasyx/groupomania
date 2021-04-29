f5d34478239e0585158bdbd4b961fd51bc1a4b5b
dasyx <seanpnn00@gmail.com>
2021-04-30 01:32:36 +0200

    update form validate + v-if models

--- frontend/src/components/Signup.vue
+++ frontend/src/components/Signup.vue
@@ -1,28 +1,54 @@
 <template>
-    <div class="form">
+    <form @submit.prevent="handleSubmit" class="form">
         <h2 class="title is-2">Inscription</h2>
 
         <div class="field">
             <label class="label">Nom</label>
             <div class="control has-icons-left has-icons-right">
-                <input class="input" type="text" placeholder="Veuillez saisir votre nom d'utilisateur" value="" />
+                <input 
+                class="input"
+                type="text"
+                v-model="userForm.name"
+                id="name"
+                name="name"
+                :class="{ 'is-invalid': isSubmitted && $v.userForm.name.$error }"
+                placeholder="Veuillez saisir votre nom d'utilisateur" 
+                value=""
+                />
                 <span class="icon is-small is-left">
                     <i class="fas fa-user"></i>
                 </span>
                 <span class="icon is-small is-right">
                 </span>
             </div>
+            <div v-if="isSubmitted && $v.userForm.name.$error">
+                <span v-if="!$v.userForm.name.required"><i class="fas fa-exclamation-triangle">Le champ nom est requis</i></span>
+                <span v-if="!$v.userForm.name.name"><i class="fas fa-exclamation-triangle">Veuillez renseigner un nom d'utilisateur valide !</i></span>
+            </div>
         </div>
 
         <div class="field">
             <label class="label">Adresse électronique</label>
             <div class="control has-icons-left has-icons-right">
-                <input class="input" type="email" placeholder="Veuillez saisir votre adresse email" value="" />
+                <input 
+                class="input" 
+                type="email" 
+                v-model="userForm.email"
+                id="email"
+                name="email"
+                :class="{ 'is-invalid': isSubmitted && $v.userForm.email.$error }"
+                placeholder="Veuillez saisir votre adresse email" 
+                value="" 
+                />
                 <span class="icon is-small is-left">
                     <i class="fas fa-envelope"></i>
                 </span>
                 <span class="icon is-small is-right">
                 </span>
+            </div>
+            <div v-if="isSubmitted && $v.userForm.email.$error" class="mail_Warning">
+                <span v-if="!$v.userForm.email.required"><i class="fas fa-exclamation-triangle">Le champ email est requis</i></span>
+                <span v-if="!$v.userForm.email.email"><i class="fas fa-exclamation-triangle">Veuillez renseigner une adresse email valide !</i></span>
             </div>
         </div>
 
@@ -56,11 +82,75 @@
                 <button class="button is-link is-light">Annuler</button>
             </div>
         </div>
-    </div>
+    </form>
 </template>
 
 <script>
+import {
+        required,
+        email,
+        minLength,
+        sameAs
+    } from 'vuelidate/lib/validators';
 
+    export default {
+        data() {
+            return {
+                userForm: {
+                    name: "",
+                    email: "",
+                    mobile: "",
+                    gender: "",
+                    password: "",
+                    confirmPassword: "",
+                    accept: ""
+                },
+                isSubmitted: false
+            };
+        },
+        validations: {
+            userForm: {
+                name: {
+                    required
+                },
+                email: {
+                    required,
+                    email
+                },
+                mobile: {
+                    required
+                },
+                gender: {
+                    required
+                },
+                password: {
+                    required,
+                    minLength: minLength(5)
+                },
+                confirmPassword: {
+                    required,
+                    sameAsPassword: sameAs('password')
+                },
+                accept: {
+                    required (val) {
+                      return val
+                    }
+                }
+            }
+        },
+        methods: {
+            handleSubmit() {
+                this.isSubmitted = true;
+
+                this.$v.$touch();
+                if (this.$v.$invalid) {
+                    return;
+                }
+
+                alert("SUCCESS!" + JSON.stringify(this.userForm));
+            }
+        }
+    };
 </script>
 
 <style lang="scss" scoped>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,10 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vuelidate from 'vuelidate';
 
 import 'bulma/css/bulma.css';
 
 require('./assets/main.scss');
+
+Vue.use(Vuelidate);
 
 Vue.config.productionTip = false
 
