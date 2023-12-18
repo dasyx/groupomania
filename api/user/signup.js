b0196702fbd47943b6edbb89362723f68dabdff0
unknown <seanpnn00@gmail.com>
2023-12-18 09:13:22 +0100

    update - add cors for all serverless functions

--- api/user/signup.js
+++ api/user/signup.js
@@ -2,9 +2,20 @@ const db = require("../../models");
 const userController = require("../../controllers/users");
 
 module.exports = async (req, res) => {
+  // Set CORS headers
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
   await db.sequelize.sync();
+
   if (req.method === "POST") {
     return userController.signup(req, res);
+  } else if (req.method === "OPTIONS") {
+    // Handle preflight request for CORS
+    res.status(200).end();
+    return;
   }
+
   res.status(405).send("Method Not Allowed");
 };
--- api/user/update.js
+++ api/user/update.js
@@ -3,10 +3,21 @@ const userController = require("../../controllers/users");
 const auth = require("../../middleware/auth");
 
 module.exports = async (req, res) => {
+  // Set CORS headers
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "PUT, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
   await db.sequelize.sync();
+
   if (req.method === "PUT") {
     await auth(req, res);
     return userController.updateUser(req, res);
+  } else if (req.method === "OPTIONS") {
+    // Handle preflight request for CORS
+    res.status(200).end();
+    return;
   }
+
   res.status(405).send("Method Not Allowed");
 };
