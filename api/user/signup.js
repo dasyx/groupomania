c94abdc7b0cdb204d51b349ba479e5f32c39fc1b
unknown <seanpnn00@gmail.com>
2023-12-18 08:36:39 +0100

    update - update api host in frontend

--- api/user/signup.js
+++ api/user/signup.js
@@ -1,11 +1,10 @@
-const db = require('../../models');
-const userController = require('../../controllers/users');
+const db = require("../../models");
+const userController = require("../../controllers/users");
 
-module.exports = (req, res) => {
-    await db.sequelize.sync();
-    if (req.method === 'POST') {
-        return userController.signup(req, res);
-    }
-    res.status(405).send('Method Not Allowed');
+module.exports = async (req, res) => {
+  await db.sequelize.sync();
+  if (req.method === "POST") {
+    return userController.signup(req, res);
+  }
+  res.status(405).send("Method Not Allowed");
 };
-
