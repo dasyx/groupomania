cea6406a01a76567116f1c78bbcaf55554ef91c6
unknown <seanpnn00@gmail.com>
2023-12-18 19:06:28 +0100

    login almost ok with database

--- api/user/signup.js
+++ api/user/signup.js
@@ -1,14 +1,20 @@
 const db = require("../../models");
 const userController = require("../../controllers/users");
 
-module.exports = async (req, res) => {
-  if (process.env.NODE_ENV === "development") {
-    await db.sequelize.sync();
-  }
+export default async function signup(req, res) {
+  try {
+    if (process.env.NODE_ENV === "development") {
+      await db.sequelize.sync();
+    }
 
-  if (req.method === "POST") {
-    return userController.signup(req, res);
-  }
+    if (req.method === "POST") {
+      return userController.signup(req, res);
+    }
 
-  res.status(405).send("Method Not Allowed");
-};
+    // Réponse pour les méthodes HTTP non gérées
+    res.status(405).send("Method Not Allowed");
+  } catch (error) {
+    console.error("Error in signup route:", error);
+    res.status(500).send("Internal Server Error");
+  }
+}
