23983b128df41f97b2078753d08f60692eee89d6
unknown <seanpnn00@gmail.com>
2023-12-18 01:26:31 +0100

    update - update api host in frontend

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const helmet = require("helmet");
+//const helmet = require("helmet");
 const path = require("path");
 
 const app = express();
@@ -8,8 +8,25 @@ const app = express();
 // Configuration CORS
 app.use(cors());
 
+const whitelistRegex =
+  /^https:\/\/groupomania-[a-zA-Z0-9]+-dasyx\.vercel\.app$/;
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    // Vérifier si l'origine est dans la whitelist ou est localhost
+    if (!origin || whitelistRegex.test(origin)) {
+      callback(null, true); // Autoriser
+    } else {
+      callback(new Error("Not allowed by CORS")); // Rejeter
+    }
+  },
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
 // Sécurité des en-têtes HTTP
-app.use(helmet());
+//app.use(helmet());
 
 // Gestion des fichiers statiques (pour les images)
 app.use("/images", express.static(path.join(__dirname, "images")));
