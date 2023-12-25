09e06b3cae65b0d9bade88cf10b54807ae3a5abf
unknown <seanpnn00@gmail.com>
2023-12-26 00:22:18 +0100

    signup & login ok + mainboard username display test

--- src/views/MainBoard.vue
+++ src/views/MainBoard.vue
@@ -69,15 +69,9 @@ const displayUserLogged = async () => {
       },
     });
 
-    if (response.status === 200 || response.status === 201) {
-      registeredUsername.value = response.data.username;
-      userLogged.value = true; // Mise à jour de l'état de connexion
-      console.log("Nom d'utilisateur enregistré:", registeredUsername.value);
-    } else {
-      console.error(
-        "Erreur lors de la récupération des informations de l'utilisateur"
-      );
-    }
+    registeredUsername.value = response.data.username;
+    userLogged.value = true; // Mise à jour de l'état de connexion
+    console.log("Nom d'utilisateur enregistré:", registeredUsername.value);
   } catch (error) {
     console.error(
       "Erreur lors de la récupération des informations de l'utilisateur:",
