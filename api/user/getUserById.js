021dba4ca8735270d90c63a20b80221ebe4eaf82
unknown <seanpnn00@gmail.com>
2023-12-25 19:19:59 +0100

    signup & login ok + mainboard username display test

--- api/user/getUserById.js
+++ api/user/getUserById.js
@@ -3,17 +3,18 @@ const userController = require("../../controllers/users");
 
 export default async function getUserById(req, res) {
   try {
-    // Synchronisation à revoir pour la production
+    // À supprimer ou à modifier pour la production
     await db.sequelize.sync();
 
     if (req.method === "GET") {
-      //const userId = req.params.id;
-      return userController.getOneUser(req, res);
+      const userId = req.params.id; // Assurez-vous d'extraire l'ID de l'utilisateur
+      return userController.getOneUser(userId, req, res); // Transmettez l'ID à la fonction du contrôleur
     } else {
       // Réponse pour les méthodes HTTP non gérées
       res.status(405).send("Method Not Allowed");
     }
   } catch (error) {
+    // Log des détails de l'erreur pour le débogage
     console.error("Error in getUserById route:", error);
     res.status(500).send("Internal Server Error");
   }
